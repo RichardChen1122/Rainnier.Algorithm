60dfa23715c5a89c86013187102a6a628ba096d9
Tu Zhanle <tzhanl@hotmail.com>
2019-06-17 23:07:42 +0800

    update

---
+++ Rainnier.Algorithm.Sort/QuickSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.Algorithm.Sort
+{
+    public class QuickSort
+    {
+        public static int Division(int[] array, int left,int right)
+        {
+            int b = array[left];
+            while (left<right)
+            {
+                while (left < right && array[right] >= b)
+                {
+                    right--;
+                }
+
+                array[left] = array[right];
+                while (left < right && array[left] <= b)
+                {
+                    left++;
+                }
+
+                array[right] = array[left];
+            }
+            array[left] = b;
+            return left;
+        }
+        public static void QuickSortMethod(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int i = Division(array, left, right);
+
+                QuickSortMethod(array, left, i - 1);
+                QuickSortMethod(array, i+1, right);
+            }
+        }
+    }
+}
